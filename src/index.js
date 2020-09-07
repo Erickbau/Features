7dbc089694a30d5e28fb5e63e758efd71bd50fdd
Erickbau <erick.skyfall10@gmail.com>
2020-09-07 20:31:22 -0300

    Testes okay

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const puppetter = require('puppeteer');
 
 //const elements = ['']
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 (async () =>{
     const browser = await puppetter.launch({headless: false});
@@ -9,8 +12,12 @@ const puppetter = require('puppeteer');
     await page.goto('https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx');
     await page.waitForSelector('div')
 
+    await page.$eval('#cboEmpresa', elem => elem.click())
     await page.focus('#cboEmpresa')
-    await page.keyboard.type('Ambev')
+    await page.keyboard.type('023264')
+    await page.waitForSelector('li#ui-id-4')
+    await page.keyboard.press('ArrowDown')
+    await page.keyboard.press('Enter')
 
     await page.$eval('input#rdPeriodo', elem => elem.click())
 
@@ -21,23 +28,32 @@ const puppetter = require('puppeteer');
     await page.keyboard.type('23:59')
 
     await page.focus('input#txtDataFim')
-    await page.keyboard.type('01/01/2010')
+    await page.keyboard.type('01/01/2020')
 
     await page.focus('input#txtHoraFim')
     await page.keyboard.type('23:59')
   
 
     await page.focus('input#cboCategoria_chosen_input')
-    await page.keyboard.type('ITR')
-    await page.keyboard.press('Enter')
+    try{
+        await page.keyboard.type('ITR', {delay: 100})
+        await page.keyboard.press('Enter')
+    }catch(err){console.log(err)}
 
+    await page.$eval('input#btnConsulta', elem => elem.click())
 
-   await browser.close()
+    await sleep(5000)
+
+    const TR = await page.$$('tbody i#VisualizarDocumento')
+    
+    console.log(TR.length)
     
 
-
+    //await browser.close()
+    
+    
     console.log('Finish')
-
+    
 })()
 
 
