ccc7e3982a841619661ed7c605b24f251559f342
Erickbau <52085450+Erickbau@users.noreply.github.com>
2020-09-09 17:52:03 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
-<<<<<<< HEAD
-mongoose.connect('mongodb://localhost/noderest',  { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, 
-=======
+
 mongoose.connect('mongodb://localhost/company',  { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, 
->>>>>>> f3e1a74d67db92a27321b6d0b37a96b2094e2a0f
 useUnifiedTopology: true });
 
 mongoose.Promise = global.Promise;
